907de884467a4bb74e3f8fa8d1bffe75f5dd8e73
student <student@DESKTOP-L3ET5DL>
2021-08-16 20:45:43 +0300

    GOOGLE_OPEN_ID

---
+++ AspNet5CookieAuth/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AspNet5CookieAuth.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.VisualBasic;
+
+namespace AspNet5CookieAuth.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult Secured()
+        {
+            return View();
+        }
+        [HttpGet("login")]
+        public IActionResult Login(string returnUrl)
+        {
+            ViewData["returnUrl"] = returnUrl;
+            return View();
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Validate(string username, string password, string returnUrl)
+        {
+            ViewData["returnUrl"] = returnUrl;
+            if (
+                username == "Bill" && password == "1"
+                || username == "John" && password == "2"
+            )
+            {
+                var claims = new List<Claim>
+                {
+                    new(ClaimTypes.NameIdentifier, username),
+                    new(ClaimTypes.Name, $"{username} {(username == "Bill" ? "Gates" : "Connor")}")
+                };
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+                await HttpContext.SignInAsync(claimsPrincipal);
+                return Redirect(returnUrl);
+            }
+            TempData["Error"] = "Error: username or password is invalid.";
+            return View("login");
+        }
+        
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return Redirect(@"https://accounts.google.com/Logout?&continue=https://appengine.google.com/_ah/logout?continue=https://localhost:5001");
+        }
+        
+        [HttpGet("denied")]
+        public IActionResult Denied()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+﻿#ASP.NET 5 Auth Demos
+
+Google Client Id
+1047106611754-b6mtjf8njk5ttbri83lshkn88gpbsisf.apps.googleusercontent.com
+
+Google Client Secret
+RrdDUGev8tSyzs9UPIeE646t
+
